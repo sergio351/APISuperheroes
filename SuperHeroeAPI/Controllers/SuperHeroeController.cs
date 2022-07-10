eac069e22042bd60466fa2961488b1ede39c05ed
sergio351 <velazquezsergio714@gmail.com>
2022-07-10 20:35:14 -0300

    first commit

---
+++ SuperHeroeAPI/Controllers/SuperHeroeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroeAPI.Models;
+
+namespace SuperHeroeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroeController : ControllerBase
+    { 
+        private static List<SuperHeroe> heroes = new List<SuperHeroe>
+        { 
+            new SuperHeroe{ 
+
+                    Id=1,
+                    Name="Spiderman",
+                    FirstName="Peter",
+                    LastName="Parker",
+                    Place="New York",
+                               
+            }
+         };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHeroe>>> Get()
+        {
+            
+            return Ok(heroes);//codigo 200 todo bien
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHeroe>>> AddHeroe(SuperHeroe heroe)
+        {
+            heroes.Add(heroe);
+            return Ok(heroes);//codigo 200 todo bien
+        }
+    }
+}
---
+++ SuperHeroeAPI/Models/SuperHeroe.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroeAPI.Models
+{
+    public class SuperHeroe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }= string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; }= string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
